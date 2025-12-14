a494d20d3da281b38caae7bd282e32c5601fda06
vladimir <vovka1998@gmail.com>
2025-12-14 13:34:06 +0300

    deleted unnecessary entities

--- bot/entities/__init__.py
+++ bot/entities/__init__.py
@@ -1,12 +1,5 @@
-from .employer import EmployerEntity
-from .resume import JobExperienceEntity, ResumeEntity
 from .user import UserEntity
-from .vacancy import VacancyEntity
 
 __all__ = [
-    "EmployerEntity",
-    "ResumeEntity",
-    "JobExperienceEntity",
     "UserEntity",
-    "VacancyEntity",
 ]
--- bot/entities/user.py
+++ bot/entities/user.py
@@ -1,24 +1,16 @@
 from pydantic import EmailStr
 
-from bot.entities import ResumeEntity
 from bot.entities.base import BaseEntity
 
 
 class UserEntity(BaseEntity):
-    hh_id: str
     name: str
     mid_name: str | None = None
     last_name: str
     phone: str | None = None
     email: EmailStr | None = None
     telegram_id: int | None = None
-    resumes: list[ResumeEntity]
 
     def __str__(self):
         mid_name = f"Отчество: {self.mid_name}\n" if self.mid_name else ""
-        return (
-            f"Имя: {self.name}\n"
-            f"{mid_name}"
-            f"Фамилия: {self.last_name}\n"
-            f"Резюме: {'\n\n'.join(str(resume) for resume in self.resumes)}"
-        )
+        return f"Имя: {self.name}\n{mid_name}Фамилия: {self.last_name}\n"
