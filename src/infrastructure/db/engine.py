eeb0a2a41081c9d82999b1ff3f43fd373ed20525
vladimir <vovka1998@gmail.com>
2025-12-15 22:24:02 +0300

    delete unnecessary

--- src/infrastructure/db/engine.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from src.infrastructure.settings.app import app_settings
-
-engine = create_async_engine(app_settings.DB.db_url)
-async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
