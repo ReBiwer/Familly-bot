d48cc0b5bcd7246027d6c27f98523fe357850932
vladimir <vovka1998@gmail.com>
2025-12-20 16:53:43 +0300

    update UserModel

--- src/db/models/user.py
+++ src/db/models/user.py
@@ -1,14 +1,15 @@
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer
 from sqlalchemy.orm import Mapped, mapped_column
+
 from src.db.models.base import BaseModel
 
 
 class UserModel(BaseModel):
     __tablename__ = "users"
 
-    name: Mapped[str] = mapped_column(String, nullable=False)
-    mid_name: Mapped[str] = mapped_column(String, nullable=True)
-    last_name: Mapped[str] = mapped_column(String, nullable=False)
-    phone: Mapped[str | None] = mapped_column(String, nullable=True)
-    email: Mapped[str | None] = mapped_column(String, nullable=True)
+    name: Mapped[str]
+    mid_name: Mapped[str]
+    last_name: Mapped[str]
+    phone: Mapped[str | None]
+    email: Mapped[str | None]
     telegram_id: Mapped[int | None] = mapped_column(Integer, unique=True, index=True, nullable=True)
