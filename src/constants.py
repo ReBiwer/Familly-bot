79b3fd1f9bf287f9b299ffcc08e64f091b9420aa
vladimir <vovka1998@gmail.com>
2025-12-23 22:23:44 +0300

    added UserRole and mapper (user role to permissions)

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,12 @@
 from enum import Enum
 
 
+class UserTole(str, Enum):
+    ADMIN = "admin"
+    MEMBER = "member"
+    CHILD = "child"
+
+
 class ScopesPermissions(str, Enum):
     USERS_READ = "users:read"
     USERS_WRITE = "users:write"
@@ -8,11 +14,23 @@ class ScopesPermissions(str, Enum):
     ADMIN = "admin"
     AI_USE = "ai:use"
 
-DEFAULT_SCOPES: list[str] = [
-    ScopesPermissions.USERS_WRITE.value,
-    ScopesPermissions.USERS_READ.value,
-    ScopesPermissions.USERS_DELETE.value,
-]
+
+ROLE_SCOPES_MAP: dict[UserTole, list[str]] = {
+    UserTole.ADMIN: [
+        ScopesPermissions.ADMIN.value,
+    ],
+    UserTole.MEMBER: [
+        ScopesPermissions.USERS_READ.value,
+        ScopesPermissions.USERS_WRITE.value,
+        ScopesPermissions.USERS_DELETE.value,
+        ScopesPermissions.AI_USE.value,
+    ],
+    UserTole.CHILD: [ScopesPermissions.USERS_READ.value, ScopesPermissions.AI_USE.value],
+}
+
+
+DEFAULT_SCOPES: list[str] = ROLE_SCOPES_MAP.get(UserTole.MEMBER)
+
 
 MEMBER_FAMILLY_SCOPE = [
     ScopesPermissions.USERS_WRITE.value,
@@ -20,6 +38,4 @@ MEMBER_FAMILLY_SCOPE = [
     ScopesPermissions.AI_USE.value,
 ]
 
-CHILD_SCOPES = [
-    ScopesPermissions.AI_USE.value
-]
+CHILD_SCOPES = [ScopesPermissions.AI_USE.value]
