665fbfff3a7eb6cfbbdf2f93e0bc8750cd7e9809
vladimir <vovka1998@gmail.com>
2026-01-15 22:15:04 +0300

    added router dialog's

--- bot/routers/__init__.py
+++ bot/routers/__init__.py
@@ -2,7 +2,8 @@ from aiogram import Router
 
 from .common import router as common_router
 from .dialogs import profile_dialog
+from .dialogs import router as dialog_router
 
 main_router = Router()
 
-main_router.include_routers(*[common_router, profile_dialog])
+main_router.include_routers(*[common_router, profile_dialog, dialog_router])
--- bot/routers/dialogs/__init__.py
+++ bot/routers/dialogs/__init__.py
@@ -1,5 +1,18 @@
-from .profile import dialog as profile_dialog, UpdateProfileSG
+from aiogram import F, Router
+from aiogram.types import CallbackQuery
+from aiogram_dialog import DialogManager, StartMode
+
+from bot.constants import ProfileActionsData
+
+from .profile import UpdateProfileSG
+from .profile import dialog as profile_dialog
+
+router = Router()
 
 
-__all__ = ["profile_dialog", "UpdateProfileSG"]
+@router.callback_query(F.data == ProfileActionsData.UPDATE)
+async def choice_update_profile(callback: CallbackQuery, dialog_manager: DialogManager):
+    await dialog_manager.start(UpdateProfileSG.main_menu, mode=StartMode.RESET_STACK)
 
+
+__all__ = ["profile_dialog", "UpdateProfileSG", "router"]
