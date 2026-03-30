9bb68979b63071d533cff9120a4a6dfff8745958
Vladimir <vovka1998@gmail.com>
2026-03-30 17:17:35 +0300

    update start message

--- bot/constants/messages.py
+++ bot/constants/messages.py
@@ -4,7 +4,13 @@ from bot.schemas import UserProfile
 class CommonMessages:
     @staticmethod
     def start_message(user_profile: UserProfile) -> str:
-        return f"Привет {user_profile.name}"
+        return (
+            f"Привет {user_profile.name}\nЯ асситент семьи Быковых\nВот список доступных команд:\n"
+            "/start - начать работу с ботом\n"
+            "/help - получить помощь\n"
+            "/profile - получить информацию о профиле\n"
+            "/agents - выбрать с кем пообщаться"
+        )
 
     @staticmethod
     def profile_message(user_profile: UserProfile) -> str:
