c126933f63511a6102fea340d261f8b024420403
Vladimir <vovka1998@gmail.com>
2026-03-30 17:17:52 +0300

    run linter

--- bot/routers/common.py
+++ bot/routers/common.py
@@ -7,10 +7,10 @@ from aiogram.fsm.context import FSMContext
 from aiogram_dialog import DialogManager
 from dishka.integrations.aiogram import FromDishka
 
-from bot.routers.dialogs import AgentChoiceSG
 from bot.adapters import BackendAdapter
 from bot.constants import CommonMessages, KeyState
 from bot.keyboards import get_actions_profile
+from bot.routers.dialogs import AgentChoiceSG
 from bot.schemas import UserProfile
 
 router = Router()
@@ -67,7 +67,11 @@ async def help_handler(message: Message):
 
 
 @router.message(Command("agents"))
-async def choice_agent(message: Message, user_profile: FromDishka[UserProfile | None], dialog_manager: DialogManager,):
+async def choice_agent(
+    message: Message,
+    user_profile: FromDishka[UserProfile | None],
+    dialog_manager: DialogManager,
+):
     if user_profile:
         await dialog_manager.start(AgentChoiceSG.choice_agent)
         return
